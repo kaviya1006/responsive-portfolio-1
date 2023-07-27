d1c1f0be7e745b3f043fb063a3540af8e4a8166e
kaviya1006 <kaviyatharshini1006@gmail.com>
2023-07-27 21:37:06 +0530

    it's me

---
+++ responsiveportfolio/javascript/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple_text", {
+  strings: ["Kaviya |", "Student |", "Developer |"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  delaySpeed: 1000,
+  loop: true,
+});
--- responsiveportfolio/script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple_text", {
-  strings: ["Kaviya |", "Student |", "Developer |"],
-  typeSpeed: 100,
-  backSpeed: 100,
-  delaySpeed: 1000,
-  loop: true,
-});
